836bf8184cef2b03e0b0e6bc0445e505311e4263
ogneslav3000 <89992293214a@gmail.com>
2023-03-24 20:48:47 +0300

    Add Presentation

--- app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
@@ -8,12 +8,19 @@ import com.example.shoppinglist2.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
+        if (count == 0) {
+            count++
 
+            val item = it[0]
+            viewModel.deleteShopItem(item)
+        }
         }
     }
 }
